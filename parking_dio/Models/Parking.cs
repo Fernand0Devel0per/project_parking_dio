198bbb804d996727670a45f1d8d46d5dec563c25
fhlottideveloper <fhlottideveloper@gmail.com>
2022-09-11 18:28:45 -0300

    create entity Vehicle and Parking

---
+++ parking_dio/Models/Parking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_dio.Models
+{
+    public class Parking
+    {
+        public decimal PriceStart { get; set; }
+        public decimal PricePerHour { get; set; }
+        public int NumberOfVacancies { get; private set; }
+        public Dictionary<string, Vehicle> Vehicle { get; set; }
+
+        public Parking(decimal priceStart, decimal pricePerHour, int numberOfVacancies)
+        {
+            PriceStart = priceStart;
+            PricePerHour = pricePerHour;
+            NumberOfVacancies = numberOfVacancies;
+        }
+    }
+}
---
+++ parking_dio/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parking_dio.Models
+{
+    public class Vehicle
+    {
+        public string LicensePlate { get; set; }
+        public DateTime EntryTime { get; set; }
+
+        public Vehicle(string licensePlate)
+        {
+            LicensePlate = licensePlate;
+            EntryTime = DateTime.Now;
+        }
+
+
+       
+    }
+}
